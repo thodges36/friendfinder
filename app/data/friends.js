ed0b2daae968a993d4588b7c7021507ab3d69593
Ted <ted_hodges@ymail.com>
2018-04-23 20:50:10 -0500

    After many issues with routes, I switched to simple HTML and adjusted routes. Now working.

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,38 +2,60 @@
 
 var friends = [
     {
-      name:"Herman E.",
+      name:"Herman Edwards",
       photo:"https://cdnph.upi.com/svc/sv/upi/5541512363528/2017/1/791c3b8651649483c320cea653559edb/Herm-Edwards-Arizona-State-officially-hires-head-coach.jpg",
       scores:[
+        "1",
+        "1",
         "5",
+        "5",
+        "5"
+      ]
+    } ,
+    {
+      name: "Bill Self",
+      photo: "https://en.wikipedia.org/wiki/File:Bill_Self,_KU.png",
+      scores: [
+        "5",
+        "3",
+        "1",
         "4",
+        "3"
+      ]
+    },
+    {
+      name: "Marty Schottenheimer",
+      photo: "https://na.rdcpix.com/228312308/8ecb8b5c10a19ccbe1daba7bc38ec77cw-c321033xd-w685_h860_q80.jpg",
+      scores: [
         "2",
+        "2",
+        "1",
+        "4",
+        "2"
+      ]
+    },
+    {
+      name: "Dick Vermeil",
+      photo: "http://www.kansascitykc.com/chiefs/dick-vermeil/dick-vermeil-chiefs.jpg",
+      scores: [
+        "4",
+        "5",
+        "1",
         "1",
         "3"
       ]
     },
     {
-      name: "Tony P.",
-      photo: "http://worldonline.media.clients.ellingtoncms.com/img/photos/2003/07/17/Pena_t440.jpg?9e2a24ba44807f8f9b96aad7c4082bf6ded075dc",
+      name: "Ned Yost",
+      photo: "https://kingsofkauffman.com/wp-content/blogs.dir/7/files/2014/05/ned-yost-mlb-kansas-city-royals-cleveland-indians.jpg",
       scores: [
         "5",
-        "1",
-        "4",
+        "2",
+        "3",
         "4",
         "5"
       ]
-    },
-    {
-      name: "Marty S.",
-      photo: "https://en.wikipedia.org/wiki/Butch_Cassidy#/media/File:Butch_Cassidy_with_bowler_hat.jpg",
-      scores: [
-        "3",
-        "2",
-        "1",
-        "3",
-        "2"
-      ]
-    }
+    } 
   ]
 
 module.exports = friends;
